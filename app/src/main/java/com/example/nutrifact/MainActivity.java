2c583adb6932c78bcc2e5de8efa2db4a65cba727
Jose Perez <jnathperez@gmail.com>
2022-04-01 11:22:52 +0800

    splashIntent

--- app/src/main/java/com/example/nutrifact/MainActivity.java
+++ app/src/main/java/com/example/nutrifact/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.nutrifact;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -10,6 +12,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_SCREEN = 5000;
+
 //Variable_animation
     Animation topAnim, botAnim;
     ImageView logo;
@@ -17,12 +21,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         //animation
-        topAnim= AnimationUtils.loadAnimation(this,R.anim.top_animation);
-        botAnim= AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        botAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
 
         //hooks
         logo = findViewById(R.id.logo);
@@ -32,6 +36,18 @@ public class MainActivity extends AppCompatActivity {
         logo.setAnimation(topAnim);
         txtLogo.setAnimation(botAnim);
         txtSlogan.setAnimation(botAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,Dashboard.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+
+
+
 
     }
 }
