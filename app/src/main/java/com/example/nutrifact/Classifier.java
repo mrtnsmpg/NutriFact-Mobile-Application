deb2c3fc6309d0f76e39d55888b90eb2465f6c24
mrtnsmpg <johnmartinsampaga@gmail.com>
2022-04-15 20:18:17 +0800

    fix message

--- app/src/main/java/com/example/nutrifact/Classifier.java
+++ app/src/main/java/com/example/nutrifact/Classifier.java
@@ -212,52 +212,52 @@ public class Classifier extends AppCompatActivity implements AdapterView.OnItemS
                     case "Apple Washington":
                         fruitName = "apple washington";
                         textFruitName.setText("Washington Apple");
-                        message = "This is washington apple. Hi Stephany, I love you.";
+                        message = "This is washington apple.";
                         break;
                     case "Apple Fuji":
                         fruitName = "apple fuji";
                         textFruitName.setText("Fuji Apple");
-                        message = "This is fuji apple. Hi Stephany, I love you.";
+                        message = "This is fuji apple.";
                         break;
                     case "Banana Lakatan":
                         fruitName = "banana lakatan";
                         textFruitName.setText("Lakatan");
-                        message = "This is lakatan. A kind of banana. Hi Stephany, I love you.";
+                        message = "This is lakatan. A kind of banana.";
                         break;
                     case "Banana Latundan":
                         fruitName = "banana latundan";
                         textFruitName.setText("Latundan");
-                        message = "This is latundan. A kind of banana. Hi Stephany, I love you.";
+                        message = "This is latundan. A kind of banana.";
                         break;
                     case "Banana Saba":
                         fruitName = "banana saba";
                         textFruitName.setText("Saba");
-                        message = "This is saba. A kind of banana. Hi Stephany, I love you.";
+                        message = "This is saba. A kind of banana.";
                         break;
                     case "Mango Carabao":
                         fruitName = "mango carabao";
                         textFruitName.setText("Carabao Mango");
-                        message = "This is carabao mango. Hi Stephany, I love you.";
+                        message = "This is carabao mango.";
                         break;
                     case "Mango Indian":
                         fruitName = "mango indian";
                         textFruitName.setText("Indian Mango");
-                        message = "This is indian mango. Hi Stephany, I love you.";
+                        message = "This is indian mango.";
                         break;
                     case "Orange":
                         fruitName = "orange";
                         textFruitName.setText("Orange");
-                        message = "This is orange. Hi Stephany, I love you.";
+                        message = "This is orange.";
                         break;
                     case "Pineapple":
                         fruitName = "pineapple";
                         textFruitName.setText("Pineapple");
-                        message = "This is pineapple. Hi Stephany, I love you.";
+                        message = "This is pineapple.";
                         break;
                     case "Watermelon":
                         fruitName = "watermelon";
                         textFruitName.setText("Watermelon");
-                        message = "This is watermelon. Hi Stephany, I love you.";
+                        message = "This is watermelon.";
                         break;
                 }
                 nutritionalValues = getNutritionalValue(fruitName);
